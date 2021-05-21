d5797443b101c383989c6319280119260f5a73b3
allexis096 <allexissoares@gmail.com>
2021-05-20 21:14:28 -0300

    :sparkles: feat: add app with childs

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Contador de Pessoas',
+      home: Stack(
+        children: [
+          Image.asset(
+            'images/restaurant.jpg',
+            fit: BoxFit.cover,
+            height: 1000,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Pessoas 0',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextButton(
+                        onPressed: () {},
+                        child: Text("+1",
+                            style:
+                                TextStyle(fontSize: 40, color: Colors.white))),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: TextButton(
+                        onPressed: () {},
+                        child: Text("-1",
+                            style:
+                                TextStyle(fontSize: 40, color: Colors.white))),
+                  ),
+                ],
+              ),
+              Text(
+                'Pode entrar',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
+          )
+        ],
+      )));
+}
